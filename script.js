fba325ce68c0f0321e4d19a71ad6419ab41a21fe
Nirmal George <73745124+ng2801@users.noreply.github.com>
2024-08-06 16:57:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "700px";
+}
+
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+}
+
+// Countdown Timer Script
+function countdown() {
+    const eventDate = new Date("January 01, 2025 00:00:00").getTime();
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML =
+       "Event Starts In " + days + " Days " + hours + "h " + minutes + "m " + seconds + "s";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}
+
+setInterval(countdown, 1000);
